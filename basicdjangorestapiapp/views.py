08833238785c8918af4c90aa0a78dd1a02395148
Gulshan <honeykulta@gmail.com>
2020-03-02 23:50:55 +0200

    Added views for articles

--- basicdjangorestapiapp/views.py
+++ basicdjangorestapiapp/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Articles
+from .serializers import ArticleSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 # Create your views here.
+
+
+@csrf_exempt
+def getAllArticle(request):
+
+    if request.method == 'GET':
+        articles = Articles.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def getArticleById(request, pk):
+    try:
+        article = Articles.objects.get(pk=pk)
+    except Articles.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)
