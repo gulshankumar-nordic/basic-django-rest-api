4fa7598444e5b9312b3ee1a9f8bb052bf15b7c40
Gulshan <honeykulta@gmail.com>
2020-03-05 00:52:18 +0200

    Added comment serializer

--- basicdjangorestapiapp/serializers.py
+++ basicdjangorestapiapp/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import Articles
+from .models import Articles, Post, Comment
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Articles
         fields = ['id', 'title', 'author']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title',  'author', 'body', 'status', 'updated']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Comment
+        fields = ['id', 'post', 'name', 'email',
+                  'body', 'created', 'updated', 'active']
